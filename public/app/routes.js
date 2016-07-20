b28012af5c94a047148ec241fa8bcf1f687e9413
LiaoJunCong <liaojuncong@foxmail.com>
2016-07-21 00:10:23 +0800

    angularjs

---
+++ public/app/routes.js
@@ -0,0 +1,25 @@
+congApp.config(["$stateProvider", "$urlRouterProvider", function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise("/dashboard");
+    $stateProvider
+        .state("dashboard", {
+            url: "/dashboard",
+            templateUrl: "/app/dashboard/dashboard.html",
+            data: { pageTitle: "dashboard" },
+            controller: "app.dashboard",
+            resolve: {
+                deps: [
+                    "$ocLazyLoad",
+                    function ($ocLazyLoad) {
+                        return $ocLazyLoad.load({
+                            name: "CongApp",
+                            serie: true,
+                            insertBefore: "#ng_load_plugins_before",
+                            files: [
+                                "/app/dashboard/dashboard.js"
+                            ]
+                        });
+                    }
+                ]
+            }
+        })
+}]);
